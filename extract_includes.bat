c8283037e8cba30daf85cf73cd36b340cfd0b895
dawnarc <aiguo1024@gmail.com>
2020-06-02 02:44:57 +0800

    add source files of version 3.12.2

---
+++ extract_includes.bat
@@ -0,0 +1,120 @@
+
+mkdir include
+mkdir include\google
+mkdir include\google\protobuf
+mkdir include\google\protobuf\compiler
+mkdir include\google\protobuf\compiler\cpp
+mkdir include\google\protobuf\compiler\csharp
+mkdir include\google\protobuf\compiler\java
+mkdir include\google\protobuf\compiler\js
+mkdir include\google\protobuf\compiler\objectivec
+mkdir include\google\protobuf\compiler\php
+mkdir include\google\protobuf\compiler\python
+mkdir include\google\protobuf\compiler\ruby
+mkdir include\google\protobuf\io
+mkdir include\google\protobuf\stubs
+mkdir include\google\protobuf\util
+copy "src\3.12.2\google\protobuf\any.h" include\google\protobuf\any.h
+copy "src\3.12.2\google\protobuf\any.pb.h" include\google\protobuf\any.pb.h
+copy "src\3.12.2\google\protobuf\api.pb.h" include\google\protobuf\api.pb.h
+copy "src\3.12.2\google\protobuf\arena.h" include\google\protobuf\arena.h
+copy "src\3.12.2\google\protobuf\arena_impl.h" include\google\protobuf\arena_impl.h
+copy "src\3.12.2\google\protobuf\arenastring.h" include\google\protobuf\arenastring.h
+copy "src\3.12.2\google\protobuf\compiler\code_generator.h" include\google\protobuf\compiler\code_generator.h
+copy "src\3.12.2\google\protobuf\compiler\command_line_interface.h" include\google\protobuf\compiler\command_line_interface.h
+copy "src\3.12.2\google\protobuf\compiler\cpp\cpp_generator.h" include\google\protobuf\compiler\cpp\cpp_generator.h
+copy "src\3.12.2\google\protobuf\compiler\csharp\csharp_generator.h" include\google\protobuf\compiler\csharp\csharp_generator.h
+copy "src\3.12.2\google\protobuf\compiler\csharp\csharp_names.h" include\google\protobuf\compiler\csharp\csharp_names.h
+copy "src\3.12.2\google\protobuf\compiler\importer.h" include\google\protobuf\compiler\importer.h
+copy "src\3.12.2\google\protobuf\compiler\java\java_generator.h" include\google\protobuf\compiler\java\java_generator.h
+copy "src\3.12.2\google\protobuf\compiler\java\java_names.h" include\google\protobuf\compiler\java\java_names.h
+copy "src\3.12.2\google\protobuf\compiler\js\js_generator.h" include\google\protobuf\compiler\js\js_generator.h
+copy "src\3.12.2\google\protobuf\compiler\js\well_known_types_embed.h" include\google\protobuf\compiler\js\well_known_types_embed.h
+copy "src\3.12.2\google\protobuf\compiler\objectivec\objectivec_generator.h" include\google\protobuf\compiler\objectivec\objectivec_generator.h
+copy "src\3.12.2\google\protobuf\compiler\objectivec\objectivec_helpers.h" include\google\protobuf\compiler\objectivec\objectivec_helpers.h
+copy "src\3.12.2\google\protobuf\compiler\parser.h" include\google\protobuf\compiler\parser.h
+copy "src\3.12.2\google\protobuf\compiler\php\php_generator.h" include\google\protobuf\compiler\php\php_generator.h
+copy "src\3.12.2\google\protobuf\compiler\plugin.h" include\google\protobuf\compiler\plugin.h
+copy "src\3.12.2\google\protobuf\compiler\plugin.pb.h" include\google\protobuf\compiler\plugin.pb.h
+copy "src\3.12.2\google\protobuf\compiler\python\python_generator.h" include\google\protobuf\compiler\python\python_generator.h
+copy "src\3.12.2\google\protobuf\compiler\ruby\ruby_generator.h" include\google\protobuf\compiler\ruby\ruby_generator.h
+copy "src\3.12.2\google\protobuf\descriptor.h" include\google\protobuf\descriptor.h
+copy "src\3.12.2\google\protobuf\descriptor.pb.h" include\google\protobuf\descriptor.pb.h
+copy "src\3.12.2\google\protobuf\descriptor_database.h" include\google\protobuf\descriptor_database.h
+copy "src\3.12.2\google\protobuf\duration.pb.h" include\google\protobuf\duration.pb.h
+copy "src\3.12.2\google\protobuf\dynamic_message.h" include\google\protobuf\dynamic_message.h
+copy "src\3.12.2\google\protobuf\empty.pb.h" include\google\protobuf\empty.pb.h
+copy "src\3.12.2\google\protobuf\extension_set.h" include\google\protobuf\extension_set.h
+copy "src\3.12.2\google\protobuf\extension_set_inl.h" include\google\protobuf\extension_set_inl.h
+copy "src\3.12.2\google\protobuf\field_mask.pb.h" include\google\protobuf\field_mask.pb.h
+copy "src\3.12.2\google\protobuf\generated_enum_reflection.h" include\google\protobuf\generated_enum_reflection.h
+copy "src\3.12.2\google\protobuf\generated_enum_util.h" include\google\protobuf\generated_enum_util.h
+copy "src\3.12.2\google\protobuf\generated_message_reflection.h" include\google\protobuf\generated_message_reflection.h
+copy "src\3.12.2\google\protobuf\generated_message_table_driven.h" include\google\protobuf\generated_message_table_driven.h
+copy "src\3.12.2\google\protobuf\generated_message_util.h" include\google\protobuf\generated_message_util.h
+copy "src\3.12.2\google\protobuf\has_bits.h" include\google\protobuf\has_bits.h
+copy "src\3.12.2\google\protobuf\implicit_weak_message.h" include\google\protobuf\implicit_weak_message.h
+copy "src\3.12.2\google\protobuf\inlined_string_field.h" include\google\protobuf\inlined_string_field.h
+copy "src\3.12.2\google\protobuf\io\coded_stream.h" include\google\protobuf\io\coded_stream.h
+copy "src\3.12.2\google\protobuf\io\gzip_stream.h" include\google\protobuf\io\gzip_stream.h
+copy "src\3.12.2\google\protobuf\io\io_win32.h" include\google\protobuf\io\io_win32.h
+copy "src\3.12.2\google\protobuf\io\printer.h" include\google\protobuf\io\printer.h
+copy "src\3.12.2\google\protobuf\io\strtod.h" include\google\protobuf\io\strtod.h
+copy "src\3.12.2\google\protobuf\io\tokenizer.h" include\google\protobuf\io\tokenizer.h
+copy "src\3.12.2\google\protobuf\io\zero_copy_stream.h" include\google\protobuf\io\zero_copy_stream.h
+copy "src\3.12.2\google\protobuf\io\zero_copy_stream_impl.h" include\google\protobuf\io\zero_copy_stream_impl.h
+copy "src\3.12.2\google\protobuf\io\zero_copy_stream_impl_lite.h" include\google\protobuf\io\zero_copy_stream_impl_lite.h
+copy "src\3.12.2\google\protobuf\map.h" include\google\protobuf\map.h
+copy "src\3.12.2\google\protobuf\map_entry.h" include\google\protobuf\map_entry.h
+copy "src\3.12.2\google\protobuf\map_entry_lite.h" include\google\protobuf\map_entry_lite.h
+copy "src\3.12.2\google\protobuf\map_field.h" include\google\protobuf\map_field.h
+copy "src\3.12.2\google\protobuf\map_field_inl.h" include\google\protobuf\map_field_inl.h
+copy "src\3.12.2\google\protobuf\map_field_lite.h" include\google\protobuf\map_field_lite.h
+copy "src\3.12.2\google\protobuf\map_type_handler.h" include\google\protobuf\map_type_handler.h
+copy "src\3.12.2\google\protobuf\message.h" include\google\protobuf\message.h
+copy "src\3.12.2\google\protobuf\message_lite.h" include\google\protobuf\message_lite.h
+copy "src\3.12.2\google\protobuf\metadata.h" include\google\protobuf\metadata.h
+copy "src\3.12.2\google\protobuf\metadata_lite.h" include\google\protobuf\metadata_lite.h
+copy "src\3.12.2\google\protobuf\parse_context.h" include\google\protobuf\parse_context.h
+copy "src\3.12.2\google\protobuf\port.h" include\google\protobuf\port.h
+copy "src\3.12.2\google\protobuf\port_def.inc" include\google\protobuf\port_def.inc
+copy "src\3.12.2\google\protobuf\port_undef.inc" include\google\protobuf\port_undef.inc
+copy "src\3.12.2\google\protobuf\reflection.h" include\google\protobuf\reflection.h
+copy "src\3.12.2\google\protobuf\reflection_ops.h" include\google\protobuf\reflection_ops.h
+copy "src\3.12.2\google\protobuf\repeated_field.h" include\google\protobuf\repeated_field.h
+copy "src\3.12.2\google\protobuf\service.h" include\google\protobuf\service.h
+copy "src\3.12.2\google\protobuf\source_context.pb.h" include\google\protobuf\source_context.pb.h
+copy "src\3.12.2\google\protobuf\struct.pb.h" include\google\protobuf\struct.pb.h
+copy "src\3.12.2\google\protobuf\stubs\bytestream.h" include\google\protobuf\stubs\bytestream.h
+copy "src\3.12.2\google\protobuf\stubs\callback.h" include\google\protobuf\stubs\callback.h
+copy "src\3.12.2\google\protobuf\stubs\casts.h" include\google\protobuf\stubs\casts.h
+copy "src\3.12.2\google\protobuf\stubs\common.h" include\google\protobuf\stubs\common.h
+copy "src\3.12.2\google\protobuf\stubs\fastmem.h" include\google\protobuf\stubs\fastmem.h
+copy "src\3.12.2\google\protobuf\stubs\hash.h" include\google\protobuf\stubs\hash.h
+copy "src\3.12.2\google\protobuf\stubs\logging.h" include\google\protobuf\stubs\logging.h
+copy "src\3.12.2\google\protobuf\stubs\macros.h" include\google\protobuf\stubs\macros.h
+copy "src\3.12.2\google\protobuf\stubs\map_util.h" include\google\protobuf\stubs\map_util.h
+copy "src\3.12.2\google\protobuf\stubs\mutex.h" include\google\protobuf\stubs\mutex.h
+copy "src\3.12.2\google\protobuf\stubs\once.h" include\google\protobuf\stubs\once.h
+copy "src\3.12.2\google\protobuf\stubs\platform_macros.h" include\google\protobuf\stubs\platform_macros.h
+copy "src\3.12.2\google\protobuf\stubs\port.h" include\google\protobuf\stubs\port.h
+copy "src\3.12.2\google\protobuf\stubs\status.h" include\google\protobuf\stubs\status.h
+copy "src\3.12.2\google\protobuf\stubs\stl_util.h" include\google\protobuf\stubs\stl_util.h
+copy "src\3.12.2\google\protobuf\stubs\stringpiece.h" include\google\protobuf\stubs\stringpiece.h
+copy "src\3.12.2\google\protobuf\stubs\strutil.h" include\google\protobuf\stubs\strutil.h
+copy "src\3.12.2\google\protobuf\stubs\template_util.h" include\google\protobuf\stubs\template_util.h
+copy "src\3.12.2\google\protobuf\text_format.h" include\google\protobuf\text_format.h
+copy "src\3.12.2\google\protobuf\timestamp.pb.h" include\google\protobuf\timestamp.pb.h
+copy "src\3.12.2\google\protobuf\type.pb.h" include\google\protobuf\type.pb.h
+copy "src\3.12.2\google\protobuf\unknown_field_set.h" include\google\protobuf\unknown_field_set.h
+copy "src\3.12.2\google\protobuf\util\delimited_message_util.h" include\google\protobuf\util\delimited_message_util.h
+copy "src\3.12.2\google\protobuf\util\field_comparator.h" include\google\protobuf\util\field_comparator.h
+copy "src\3.12.2\google\protobuf\util\field_mask_util.h" include\google\protobuf\util\field_mask_util.h
+copy "src\3.12.2\google\protobuf\util\json_util.h" include\google\protobuf\util\json_util.h
+copy "src\3.12.2\google\protobuf\util\message_differencer.h" include\google\protobuf\util\message_differencer.h
+copy "src\3.12.2\google\protobuf\util\time_util.h" include\google\protobuf\util\time_util.h
+copy "src\3.12.2\google\protobuf\util\type_resolver.h" include\google\protobuf\util\type_resolver.h
+copy "src\3.12.2\google\protobuf\util\type_resolver_util.h" include\google\protobuf\util\type_resolver_util.h
+copy "src\3.12.2\google\protobuf\wire_format.h" include\google\protobuf\wire_format.h
+copy "src\3.12.2\google\protobuf\wire_format_lite.h" include\google\protobuf\wire_format_lite.h
+copy "src\3.12.2\google\protobuf\wrappers.pb.h" include\google\protobuf\wrappers.pb.h
--- test/build-windows.bat
+++ test/build-windows.bat
@@ -7,6 +7,6 @@ mkdir build & cd build
 
 mkdir release & cd release
 
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=release ../..
+cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=release -DPROTOBUF_VERSION}=3.12.2
 
 nmake
